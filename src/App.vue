2e07a1c2f68f1018b72a8c09f004c94f40c5a5ae
Simon Dahal <simonsd054@gmail.com>
2018-07-20 17:51:10 +0545

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app id="ecommerce">
+
+    <v-toolbar
+      app
+      dense
+    >
+      <v-menu offset-y attach="#menu">
+        <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in items"
+            :key="index"
+            @click=""
+          >
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <div id="menu"></div>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer fixed app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      title: 'Ecommerce App'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import store from './store'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: {App},
+  template: '<App/>'
+})
